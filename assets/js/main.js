d827377a3ae957d2b2293f9b0f581c6d67c02538
spoilss <spoilssbor@gmail.com>
2017-02-18 19:58:49 +0200

    exam4

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$('.lorem-slider').slick({
+		dots: false,
+		infinite: true,
+		speed: 700,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		arrows: true,
+	});
+
+});
